815f72564787b481aa0852fe8677986401a93880
Mark Williams <amwilliams137@gmail.com>
2024-12-06 19:32:16 +0000

    Refactor

--- dotnet-solutions/Day3.cs
+++ dotnet-solutions/Day3.cs
@@ -4,18 +4,22 @@ namespace AoC2024;
 
 public class Day3
 {
-    public static int Part1()
+    public static int Part1()    
+        => GetMulSumForText(File.ReadAllText("../inputs/3.txt"));
+    
+
+    public static int Part2()
     {
-        var result = 0;
-        var text = File.ReadAllText("../inputs/3.txt");
-        var matches = Regex.Matches(text, @"mul\(\d+\,\d+\)");
+        // Split on do()
+        // Then split those bits on don't() - ignore everything after the first "chunk"
 
-        foreach (Match match in matches)
-        {
-            var ints = Regex.Matches(match.Value, @"\d+").ToArray();
-            result += int.Parse(ints[0].Value) * int.Parse(ints[1].Value);
-        }
-
-        return result;
+        return 0;
     }
+
+    private static int GetMulSumForText(string text)
+        => Regex.Matches(text, @"mul\(\d+\,\d+\)")
+            .Sum(match => {
+                var ints = Regex.Matches(match.Value, @"\d+").ToArray();
+                return int.Parse(ints[0].Value) * int.Parse(ints[1].Value);
+            });
 }
