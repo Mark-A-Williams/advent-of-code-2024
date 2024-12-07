4615f1cc14072ad405cd64ab836af587c88dc7dd
Mark Williams <amwilliams137@gmail.com>
2024-12-07 14:39:39 +0000

    4.2

--- dotnet-solutions/Day4.cs
+++ dotnet-solutions/Day4.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace AoC2024;
 
 public class Day4
@@ -25,7 +27,18 @@ public class Day4
 
     public static int Part2()
     {
-        return 0;
+        var grid = File.ReadAllLines("../inputs/4.txt").Select(line => line.ToArray()).ToArray();
+        var masXCount = 0;
+
+        for (var y = 0; y < grid.Length; y++)
+        {
+            for (var x = 0; x < grid[y].Length; x++)
+            {
+                if (IsCenterOfMasX(grid, (x, y))) masXCount++;
+            }
+        }
+
+        return masXCount;
     }
 
     // We'll only call this (for every direction) if we're on an X 
@@ -50,5 +63,28 @@ public class Day4
         }
 
         return true;
+    }
+
+    private static bool IsCenterOfMasX(
+        char[][] grid,
+        (int X, int Y) startCoords)
+    {
+        var gridWidth = grid[0].Length;
+        var gridHeight = grid.Length;
+
+        if (grid[startCoords.Y][startCoords.X] != 'A') return false;
+
+        if (startCoords.X < 1 || startCoords.X >= gridWidth - 1 || startCoords.Y < 1 || startCoords.Y >= gridHeight - 1) return false;
+
+        var topLeft = grid[startCoords.Y - 1][startCoords.X - 1];
+        var topRight = grid[startCoords.Y - 1][startCoords.X + 1];
+        var bottomLeft = grid[startCoords.Y + 1][startCoords.X - 1];
+        var bottomRight = grid[startCoords.Y + 1][startCoords.X + 1];
+
+        char[] positiveDiagonal = [topLeft, bottomRight];
+        char[] negativeDiagonal = [topRight, bottomLeft];
+
+        return positiveDiagonal.Count(o => o == 'S') == 1 && positiveDiagonal.Count(o => o == 'M') == 1
+            && negativeDiagonal.Count(o => o == 'S') == 1 && negativeDiagonal.Count(o => o == 'M') == 1;
     }
 }
